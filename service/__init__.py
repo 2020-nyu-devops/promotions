4255c06b30fc563e6e4f712b8d4326c19ab7fb53
Ling-I Huang <liondonaugust810810@gmail.com>
2020-10-15 16:57:16 -0400

    create initializer(__init__.py) for service package

---
+++ service/__init__.py
@@ -0,0 +1,32 @@
+"""
+Package: service
+Package for the application models and service routes
+This module creates and configures the Flask app and sets up the logging
+and SQL database
+"""
+import os
+import sys
+import logging
+from flask import Flask
+
+# Create Flask application
+app = Flask(__name__)
+app.config.from_object('config')
+
+# Import the routes After the Flask app is created
+from service import service, models
+
+# Set up logging for production #TODO
+
+app.logger.info(70 * "*")
+app.logger.info("  P R O M O   S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(70 * "*")
+
+try:
+    service.init_db()  # make our sqlalchemy tables
+except Exception as error:
+    app.logger.critical("%s: Cannot continue", error)
+    # gunicorn requires exit code 4 to stop spawning workers when they die
+    sys.exit(4)
+
+app.logger.info("Service inititalized!")
